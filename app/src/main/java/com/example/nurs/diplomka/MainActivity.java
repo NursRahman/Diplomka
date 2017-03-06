57c636352fd94964915e3eea256e95d2808c9343
NursRahman <nursultanrahman@gmail.com>
2017-03-06 10:05:10 +0600

    Comment

---
+++ app/src/main/java/com/example/nurs/diplomka/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.nurs.diplomka;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageButton tustorButton;
+    ImageButton sandarButton;
+    ImageButton formaButton;
+    ImageButton alippeButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
